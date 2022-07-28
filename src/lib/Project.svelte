50c5bce44e1cb1515a462467d97a952e3208a84d
Florian Kuc <florian.kuc@gmail.com>
2022-07-29 00:03:35 +0200

    style(projects): style projects

--- src/lib/Project.svelte
+++ src/lib/Project.svelte
@@ -1,26 +1,48 @@
 <script lang="ts">
-  import type { ProjectProps } from "src/routes/projects.svelte";
-import Link from "./Link.svelte";
-  export let project: ProjectProps;
+	import type { ProjectProps } from 'src/routes/projects.svelte';
+	import Link from './Link.svelte';
+	export let project: ProjectProps;
 
-  console.log(project)
+	console.log(project);
 </script>
-<!-- 
-topics: string[];
-gh_url: string;
-web_url: string; -->
 
-<li>
-    <h3>{project.name.split(/[-_]/g).join(" ")}</h3>
-    <p>{project.description}</p>
-    <ul>
-      {#each project.topics as topic}
-        <p>{topic}</p>
-      {/each}
-    </ul>
-    <a href={project.html_url} target="_blank"><Link/></a>
+<li class="project">
+	<div>
+		<h3>{project.name.split(/[-_]/g).join(' ')}</h3>
+		<p>{project.description}</p>
+		<ul>
+			{#each project.topics as topic}
+				<li>{topic}</li>
+			{/each}
+		</ul>
+	</div>
+	<a href={project.html_url} target="_blank"><Link /></a>
 </li>
 
-<style>
-  /* your styles go here */
+<style lang="scss">
+	@import '../styles/variables.scss';
+	.project {
+		display: flex;
+		background-color: $color-grey-light-mid;
+		border-radius: 1rem;
+		padding: 1rem;
+
+		div {
+			flex-grow: 1;
+
+			ul {
+				flex-wrap: wrap;
+				margin: 1rem 0 0 1rem;
+				gap: 1rem;
+
+				li {
+					font-size: 0.9rem;
+					border-radius: 0.3rem;
+					padding: 0.2rem 0.5rem;
+					background-color: $color-black;
+					color: $color-white;
+				}
+			}
+		}
+	}
 </style>
--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -1,49 +1,54 @@
 <script context="module" lang="ts">
-  import type { Load } from "@sveltejs/kit";
+	import type { Load } from '@sveltejs/kit';
 
-  export const load: Load = async ({fetch}) => {
-    const response = await fetch('https://api.github.com/users/floriankuc/repos?sort=created&direction=desc')
-    const projects = await response.json()
-  
-    return {
-      props: {
-        projects
-      }
-    }
-  }
+	export const load: Load = async ({ fetch }) => {
+		const response = await fetch(
+			'https://api.github.com/users/floriankuc/repos?sort=created&direction=desc'
+		);
+		const projects = await response.json();
 
-  export interface ProjectProps{
-    name: string;
-    description: string;
-    topics: string[];
-    html_url: string;
-  }
+		return {
+			props: {
+				projects
+			}
+		};
+	};
+
+	export interface ProjectProps {
+		name: string;
+		description: string;
+		topics: string[];
+		html_url: string;
+	}
 </script>
 
 <script lang="ts">
-  import Project from '../lib/Project.svelte'
-  export let projects: ProjectProps[];
-  const l = projects.length
+	import Project from '../lib/Project.svelte';
+	export let projects: ProjectProps[];
+	const l = projects.length;
 </script>
 
-<p> 
-  These are some projects I have worked on outside work, sourced from
-  my <a href="https://api.github.com/users/floriankuc" target="_blank">
-    Github profile
-  </a>
-  and generally work in progress. Some on the verge of
-  completion, others I may have abandoned entirely in the light of
-  something more captivating. 😇
+<p>
+	These are some projects I have worked on outside work, sourced from my <a
+		href="https://github.com/floriankuc"
+		target="_blank"
+	>
+		Github profile
+	</a>
+	and generally work in progress. Some on the verge of completion, others I may have abandoned entirely
+	in the light of something more captivating. 😇
 </p>
 
 <ul>
-{#each projects as p}
-  <Project project={p}/>
-{/each}
+	{#each projects as p}
+		<Project project={p} />
+	{/each}
 </ul>
 
 <style lang="scss">
-  ul {
-    flex-direction: column;
-  }
+	ul {
+		margin-top: 5rem;
+		flex-direction: column;
+		gap: 3rem;
+	}
 </style>
